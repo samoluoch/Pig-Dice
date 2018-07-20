fff4885ea5d087885bb13c56e77d86400a7a8e91
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-20 15:47:50 +0300

    added labels to the two dice results and restyled

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
       $function rollDice() {
         var dice1 = document.getElementById("dice1");
         var dice2 = document.getElementById("dice2");
-        var Outcome = document.getElementById("outcome");
         var diceOne = Math.floor(Math.random()*6) + 1;
         var diceTwo = Math.floor(Math.randon()*6) + 1;
         var totalDice = diceOne + diceTwo;
+        var Outcome = document.getElementById("outcome");
         dice1.innerHTML = diceOne;
         dice2.innerHTML = diceTwo;
+        $("#outcome").append("totalDice");
       };
 });
 });
