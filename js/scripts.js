828f71c05d8382f6d5ef59319c3dd92d129b251a
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-20 11:05:45 +0300

    added a function that will create a value for the variable dice2 after clicking and a random value generated, multiplied by 6 and then 1 added to it

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
       $function rollDice() {
         var dice1 = Math.floor(Math.random()*6) + 1;
         var dice2 = Math.floor(Math.randon()*6) + 1;
-      }
-
-})
+      });
+});
+});
