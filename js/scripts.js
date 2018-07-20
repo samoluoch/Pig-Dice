718926ec9f5131ba477df2786f0267fbc25ce335
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-20 12:23:51 +0300

    added a function that communicates with the #dice1 and #dice2

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
     $("button").click(function(event) {
       event.preventDefault();
       $function rollDice() {
-        var dice1 = Math.floor(Math.random()*6) + 1;
-        var dice2 = Math.floor(Math.randon()*6) + 1;
+        var dice1 = document.getElementById("dice1");
+        var dice2 = document.getElementById("dice2");
+        var diceOne = Math.floor(Math.random()*6) + 1;
+        var diceTwo = Math.floor(Math.randon()*6) + 1;
+        var totalDice = dice1 + dice2;
+
+
       });
 });
 });
