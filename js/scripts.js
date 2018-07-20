25847da97b8060a21ec06012f06b8119cb54e82a
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-07-20 15:10:53 +0300

    added rules for the players. changed button id to bt

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-    $("button").click(function(event) {
+    $("button#bt").click(function(event) {
       event.preventDefault();
       $function rollDice() {
         var dice1 = document.getElementById("dice1");
         var dice2 = document.getElementById("dice2");
+        var Outcome = document.getElementById("outcome");
         var diceOne = Math.floor(Math.random()*6) + 1;
         var diceTwo = Math.floor(Math.randon()*6) + 1;
-        var Outcome = document.getElementById("outcome");
         var totalDice = diceOne + diceTwo;
         dice1.innerHTML = diceOne;
         dice2.innerHTML = diceTwo;
-
-
-      });
+      };
 });
 });
